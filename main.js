029958eaac1985da5caaff6e5a450264341cca6a
Philipp Otto <philipp@scm.io>
2014-08-06 23:19:44 +0200

    add examples and better error handling

--- main.js
+++ main.js
@@ -2,11 +2,11 @@ $(document).ready(function() {
   var editor = ace.edit("editor");
   var inputArea = $("#input-area");
   var outputArea = $("#output-area");
+  var errorAlert = $("#error-alert");
 
   setupEditor();
   setupInputArea();
   restoreValues();
-  evalCode();
 
   function setupEditor() {
     editor.commands.addCommand({
@@ -23,38 +23,69 @@ $(document).ready(function() {
     // editor.setTheme("ace/theme/monokai");
     editor.getSession().setMode("ace/mode/javascript");
 
-    editor.getSession().on('change', function() {
-      saveCode();
-      evalCode();
-    });
+    editor.getSession().on('change', saveAndEval);
   }
 
   function restoreValues() {
-    inputArea.val(localStorage.getItem("fiddlerInput"));
-    editor.setValue(localStorage.getItem("fiddlerCode"));
+    var input = localStorage.getItem("fumblerInput");
+        code  = localStorage.getItem("fumblerCode");
+
+    if (!input && !code) {
+      fetchAndLoadExample();
+    } else {
+      inputArea.val(input);
+      editor.setValue(code);
+    }
+
+    saveAndEval();
   }
 
   function evalCode() {
+    // make the input accessible via global namespace,
+    // so that the script can access it
     window.input = inputArea.val();
     var code = editor.getValue();
     try {
-      if (!window.noAutoEval)
+      if (!window.noAutoEval) {
         outputArea.val(eval(code));
+      }
+      hideError();
     } catch (e) {
       console.error("an error occured while executing your code",  e)
+      showError(e);
     }
   }
 
-  function saveCode() {
-    var code = editor.getValue();
-    localStorage.setItem("fiddlerCode", code);
+  function save() {
+    localStorage.setItem("fumblerCode", editor.getValue());
+    localStorage.setItem("fumblerInput", inputArea.val());
+  }
+
+  function saveAndEval() {
+    save();
+    evalCode();
   }
 
   function setupInputArea() {
-    inputArea.on("input", function() {
-      localStorage.setItem("fiddlerInput", inputArea.val());
-      evalCode();
-    });
+    inputArea.on("input", saveAndEval);
+  }
+
+  function showError(err) {
+    errorAlert.text("JavaScript exception: " + err);
+    errorAlert.show();
+  }
+
+  function hideError() {
+    errorAlert.hide();
+  }
+
+  function loadExample(example) {
+    inputArea.val(example.input);
+    editor.setValue(example.code);
+  }
+
+  function fetchAndLoadExample() {
+    jQuery.get("examples/html-to-coffee-react.json", loadExample, "json");
   }
 
 });
